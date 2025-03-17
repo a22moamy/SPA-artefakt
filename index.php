ce0c33453282b1d5158e39ae1361cc95ea7df69c
moamy <moamyrs@gmail.com>
2025-03-17 20:05:45 +0100

    Added three pages (Elements)
    Added three elements which will become three websites within the SPA. Some styling to differentiate these has also been added.

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Single Page Application</title>
+    <link rel="stylesheet" href="css/page.css">
 </head>
 <body>
     <nav>
@@ -17,5 +18,17 @@
             <a href="">WEBP</a>
         </button>
     </nav>
+    <div id="jpg" class="page">
+        <h2>JPG Image gallery</h2>
+        <div class="imageGroup"></div>
+    </div>
+    <div id="png" class="page">
+        <h2>PNG Image gallery</h2>
+        <div class="imageGroup"></div>
+    </div>
+    <div id="webp" class="page">
+        <h2>WEBP Image gallery</h2>
+        <div class="imageGroup"></div>
+    </div>
 </body>
 </html>
