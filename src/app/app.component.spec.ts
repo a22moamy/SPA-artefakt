a122389b04cdd2dd766f2b9bba9e4cea18e28f54
moamy <a22moamy@student.his.se>
2025-05-13 11:28:53 +0200

    Updated Corrupted Angular Files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'spa-temp' title`, () => {
+  it(`should have the 'SPA' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('spa-temp');
+    expect(app.title).toEqual('SPA');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, spa-temp');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, SPA');
   });
 });
